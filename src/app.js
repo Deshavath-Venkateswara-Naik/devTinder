7e84e65ec0272a49d38fa9eab237b1c713ca2d10
Deshavath-Venkateswara-Naik <deshavathvenkateswaranaik0193@gmail.com>
2025-05-23 12:31:00 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require("express");
  
 const app = express();
-app.use("/test",(req,res)=>{
-    res.send("Hello world");
-});
+app.use("/user",(req,res,next)=>{
+    console.log("hello");
+    next();
+},(req,res)=>{
+    res.send("ok");
+}
+);
 app.listen(3000,()=>{
     console.log("Server is running on port 3000....");
 });
