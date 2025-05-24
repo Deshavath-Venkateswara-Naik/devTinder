eb592fecf502b3f28f0bbd2b08e3177a97e891d0
Deshavath-Venkateswara-Naik <deshavathvenkateswaranaik0193@gmail.com>
2025-05-24 22:08:00 +0530

    Ayth including

--- src/app.js
+++ src/app.js
@@ -3,11 +3,12 @@ const express = require("express");
 const app = express();
 app.use("/user",(req,res,next)=>{
     console.log("hello");
+    res.send("no");
     next();
 },(req,res)=>{
     res.send("ok");
 }
-);
+); 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000....");
 });
