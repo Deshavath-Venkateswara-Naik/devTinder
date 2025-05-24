f2f06716b5a78231c651c8e25271339d04412727
Deshavath-Venkateswara-Naik <deshavathvenkateswaranaik0193@gmail.com>
2025-05-24 22:08:34 +0530

    auth in

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 const express = require("express");
- 
+
 const app = express();
-app.use("/user",(req,res,next)=>{
-    console.log("hello");
-    res.send("no");
-    next();
-},(req,res)=>{
-    res.send("ok");
-}
-); 
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000....");
+const { adminAuth } = require("./middlewares/auth");
+
+// Apply adminAuth middleware to all routes under /admin
+app.use("/admin", adminAuth);
+
+// Route for /admin/getData
+app.use("/admin/getData", (req, res, next) => {
+    res.send("Data fetched successfully");
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000...");
 });
---
+++ src/middlewares/auth.js
@@ -0,0 +1,14 @@
+const adminAuth = (req,res,next)=>{
+    const token = "xyz";
+    const isAdmin = token === "xyz";
+    if(!isAdmin){
+        res.status(401).send("unauthorizes request");    
+    }
+    else{
+        next();
+    }
+};
+
+module.exports ={
+    adminAuth
+}
