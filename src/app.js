0a63437106e9d141cca4943aa1b987969ee66862
Deshavath-Venkateswara-Naik <deshavathvenkateswaranaik0193@gmail.com>
2025-06-10 23:25:45 +0530

    login api created with credentials

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const express = require("express");
-
 const app = express();
 const { adminAuth } = require("./middlewares/auth");
 const connectDB = require("./config/database");
 const User= require("./models/user");
-
+const {validateSignUpData} = require('./utils/validator');
+const bcrypt = require('bcrypt');
 app.use(express.json());
 
 app.get("/signup",async(req,res)=>{
@@ -18,13 +18,40 @@ app.get("/signup",async(req,res)=>{
    });
 app.post("/signup",async(req,res)=>{
 try{
-    const user = await User(req.body);
+    // validate
+    validateSignUpData(req);
+    // encryption
+    const { firstName, lastName, emailId, password } = req.body;
+    const passwordHash = await bcrypt.hash(password,10);
+    // Saving the user
+    const user = await User({
+        firstName,
+        lastName,
+        emailId,
+        password: passwordHash
+    });
     await user.save();
     res.send(user);
 }catch(error){
     res.status(400).send("error saving the user:"+error.message);
 }
 });
+app.post("/login",async(req,res)=>{
+    const {emailId,password}=req.body;
+    const user = await User.findOne({emailId:emailId});
+    console.log(user);
+    if(!user){
+        throw new Error('user not found in DB');
+    } 
+    const pass = await bcrypt.compare(password,user.password)
+    if(!pass){
+        throw new Error("password is incorrect");
+    }
+    else
+    {
+        res.send("user fetched successfully");
+    }
+})
 
 app.put("/signup",async(req,res)=>{
     const userEmail =req.body.emailId;
